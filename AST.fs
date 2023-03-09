67c4eab866b651e0dadcf40601aa3e6a33390c60
jakob_kild <s214952@dtu.dk>
2023-03-09 16:04:16 +0100

    begyndelse på AST

--- AST.fs
+++ AST.fs
@@ -2,22 +2,10 @@
 // to store represent arithmetic expressions
 module AST
 
-type Command =
-    | Skip
-    | Sequence of Command * Command
-
-//C  ::=  x := a  |  A[a] := a  |  skip  |  C ; C  |  if GC fi  |  do GC od
-
-//GC ::=  b -> C  |  GC [] GC
-
-//a  ::=  n  |  x  |  A[a]  |  a + a  |  a - a  |  a * a  |  a / a  |  - a  |  a ^ a  |  (a)
-
-//b  ::=  true  |  false  |  b & b  |  b | b  |  b && b  |  b || b  |  ! b
-//     |  a = a  |  a != a  |  a > a  |  a >= a  |  a < a  |  a <= a  |  (b)
-
-
+//a  ::=  n  |  x  |  A[a]  |  a + a  |  a - a  |  a * a  |  a / a  |  - a  |  a ^ a  |  (a)    
 type expr =
     | Num of float
+    | Variable of string
     | TimesExpr of (expr * expr)
     | DivExpr of (expr * expr)
     | PlusExpr of (expr * expr)
@@ -25,3 +13,29 @@ type expr =
     | PowExpr of (expr * expr)
     | UPlusExpr of (expr)
     | UMinusExpr of (expr)
+
+//C  ::=  x := a  |  A[a] := a  |  skip  |  C ; C  |  if GC fi  |  do GC od
+type Command =
+    | Declaration of (expr * expr)
+    | Skip
+    | Sequence of (Command * Command)
+    
+//GC ::=  b -> C  |  GC [] GC
+type GuardedCommand =
+    | BooleanExpr of Command
+
+//b  ::=  true  |  false  |  b & b  |  b | b  |  b && b  |  b|| b  |  ! b
+//     |  a = a  |  a != a  |  a > a  |  a >= a  |  a < a  |  a <= a  |  (b)
+type BooleanExpr =
+    | True
+    | False
+    | BoolAnd of (BooleanExpr * BooleanExpr)
+    | BoolOr of (BooleanExpr * BooleanExpr)
+    | BoolNot of (BooleanExpr)
+    | BoolEqual of (expr * expr)
+    | BoolNotEqual of (expr * expr)
+    | BoolGreater of (expr * expr)
+    | BoolGreaterOrEqual of (expr * expr)
+    | BoolLess of (expr * expr)
+    | BoolLessOrEqual of (expr * expr)
+    

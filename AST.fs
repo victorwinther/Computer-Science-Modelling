702a6d8abf2475a3358f34cc9383e7d86a37b232
jakob_kild <s214952@dtu.dk>
2023-03-09 17:08:25 +0100

    fixed

--- AST.fs
+++ AST.fs
@@ -1,27 +1,6 @@
 // This file implements a module where we define a data type "expr"
 // to store represent arithmetic expressions
 module AST
-//C  
-type Command =
-// ::=  x := a  
-// |  A[a] := a  
-    | Skip // |  skip  
-    | Sequence of Command * Command// |  C ; C  
-// |  if GC fi  
-// |  do GC od
-
-//GC ::=  b -> C  |  GC [] GC
-//  type GuardedCommand
-
-
-
-//a  ::=  n  |  x  |  A[a]  |  a + a  |  a - a  |  a * a  |  a / a  |  - a  |  a ^ a  |  (a)
-//  type ArithmemeticExpression jakob her
-
-
-//b  ::=  true  |  false  |  b & b  |  b | b  |  b && b  |  b || b  |  ! b
-//     |  a = a  |  a != a  |  a > a  |  a >= a  |  a < a  |  a <= a  |  (b)
-//  type BooleanExpression
 
 //a  ::=  n  |  x  |  A[a]  |  a + a  |  a - a  |  a * a  |  a / a  |  - a  |  a ^ a  |  (a)    
 type expr =
@@ -65,6 +44,8 @@ type Command =
 and GuardedCommand =
     | Condition of (BoolExpr * Command)
     | Else of (GuardedCommand * GuardedCommand)
+
+
 
 
 

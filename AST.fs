48f81973d1a2e8fce6d2e533ba563d1082efa83b
VicAceCase <victor.larsen5@hotmail.com>
2023-03-09 16:07:21 +0100

    victor changes  Changes to be committed:
    victor

--- AST.fs
+++ AST.fs
@@ -1,6 +1,27 @@
 // This file implements a module where we define a data type "expr"
 // to store represent arithmetic expressions
 module AST
+//C  
+type Command =
+// ::=  x := a  
+// |  A[a] := a  
+    | Skip // |  skip  
+    | Sequence of Command * Command// |  C ; C  
+// |  if GC fi  
+// |  do GC od
+
+//GC ::=  b -> C  |  GC [] GC
+//  type GuardedCommand
+
+
+
+//a  ::=  n  |  x  |  A[a]  |  a + a  |  a - a  |  a * a  |  a / a  |  - a  |  a ^ a  |  (a)
+//  type ArithmemeticExpression
+
+
+//b  ::=  true  |  false  |  b & b  |  b | b  |  b && b  |  b || b  |  ! b
+//     |  a = a  |  a != a  |  a > a  |  a >= a  |  a < a  |  a <= a  |  (b)
+//  type BooleanExpression
 
 type expr =
     | Num of float
